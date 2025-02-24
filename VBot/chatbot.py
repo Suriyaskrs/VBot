5ec44ff021332ef8b0477590f904e70bae8c94c5
Suriyaskrs <suriyaskrs@gmail.com>
2025-02-24 08:50:13 +0530

    Update chatbot.py

--- VBot/chatbot.py
+++ VBot/chatbot.py
@@ -23,7 +23,7 @@ stop_words = set(stopwords.words('english'))
 
 def initialize_chatbot():
     global sent_tokens, word_tokens, stop_words
-
+    nltk.download('punkt_tab') #comment it after running 1 time
     # Load and process data
     #replace your file path accordingly
     # Load and process data from GitHub
